ef51da9d41102a879ad647c19ec241ac73975270
KasjanychevMikhail <43135863+KasjanychevMikhail@users.noreply.github.com>
2018-12-27 08:42:22 +0300

    Update main.c

--- kasjanychev_mp/Practice/practice2/main.c
+++ kasjanychev_mp/Practice/practice2/main.c
@@ -21,35 +21,38 @@ void main()
 	}
 	while (z != n)
 	{
-        printf("Vvedite predpolozhitelnoe chislo");
-	    scanf("%d", &y);
-        for (i = 1; i < n + 1; i++)
-            z[i] = y / i;
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                if (z[i] == z[j]) {
-                    printf("Chislo soderzhit povtoryaushiesya cifri");
-                    return;
-                }
-            }
-        }
-	    m = 10 * n;
-	    for (i = 0; i < n; i++)
-	    {
-		    x[i] = y / m;
-		    y = y % m;
-		    m = m / 10;
-	    }
-	    for (i = 0; i < n; i++)
-	    {
-		    for (j = 0; j < n; j++)
-		    {
-		    	if ((x[i] == b[j]) && (i != j))
-				k++;
-	    		if ((x[i] == b[j]) && (i == j))
-				z++;
-	    	}
-    	}
+		printf("Vvedite predpolozhitelnoe chislo");
+		scanf("%d", &y);
+		for (i = 1; i < n + 1; i++)
+			z[i] = y / i;
+		for (i = 0; i < n; i++) 
+		{
+			for (j = 0; j < n; j++) 
+			{
+				if (z[i] == z[j]) 
+				{
+					printf("Chislo soderzhit povtoryaushiesya cifri");
+					return;
+				}
+			}
+		}
+		m = 10 * n;
+		for (i = 0; i < n; i++)
+		{
+			x[i] = y / m;
+			y = y % m;
+			m = m / 10;
+		}
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if ((x[i] == b[j]) && (i != j))
+					k++;
+				if ((x[i] == b[j]) && (i == j))
+					z++;
+			}
+		}
 		printf("Kol-vo korov=%d", k);
 		printf("Kol-vo bikov=%d", z);
 	}
