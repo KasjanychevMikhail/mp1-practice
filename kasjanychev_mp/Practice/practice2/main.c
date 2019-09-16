b45ad85aa1dd0c67c9ed1dacf2c1c436070cbe06
KasjanychevMikhail <43135863+KasjanychevMikhail@users.noreply.github.com>
2019-09-16 15:32:34 +0300

    Update main.c

--- kasjanychev_mp/Practice/practice2/main.c
+++ kasjanychev_mp/Practice/practice2/main.c
@@ -6,7 +6,7 @@ void main()
 	int n, l, y = 0;
 	int a[10], b[10], x[10], z[10];
 	int i, m, j;
-	int j, k, z = 0;
+	int j, k, zz = 0;
 	printf("Vvedite dlinu zagadivaemogo chisla");
 	scanf("%d", &n);
 	srand((unsigned int)time(0));
@@ -14,44 +14,47 @@ void main()
 		a[i] = i;
 	for (l = 0; l < n; l++)
 	{
-		i = 9 / RAND_MAX*rand();
+		i = 9 / RAND_MAX * rand();
 		b[l] = a[i];
 		for (i = i; i < 10; i++)
 			a[i] = a[i + 1];
 	}
 	while (z != n)
 	{
-        printf("Vvedite predpolozhitelnoe chislo");
-	    scanf("%d", &y);
-        for (i = 1; i < n + 1; i++)
-            z[i] = y / i;
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                if (z[i] == z[j]) {
-                    printf("Chislo soderzhit povtoryaushiesya cifri");
-                    return;
-                }
-            }
-        }
-	    m = 10 * n;
-	    for (i = 0; i < n; i++)
-	    {
-		    x[i] = y / m;
-		    y = y % m;
-		    m = m / 10;
-	    }
-	    for (i = 0; i < n; i++)
-	    {
-		    for (j = 0; j < n; j++)
-		    {
-		    	if ((x[i] == b[j]) && (i != j))
-				k++;
-	    		if ((x[i] == b[j]) && (i == j))
-				z++;
-	    	}
-    	}
+		printf("Vvedite predpolozhitelnoe chislo");
+		scanf("%d", &y);
+		for (i = 1; i < n + 1; i++)
+			z[i] = y / i;
+		for (i = 0; i < n; i++) 
+		{
+			for (j = 0; j < n; j++) 
+			{
+				if (z[i] == z[j]) 
+				{
+					printf("Chislo soderzhit povtoryaushiesya cifri");
+					return;
+				}
+			}
+		}
+		m = 10 * n;
+		for (i = 0; i < n; i++)
+		{
+			x[i] = y / m;
+			y = y % m;
+			m = m / 10;
+		}
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if ((x[i] == b[j]) && (i != j))
+					k++;
+				if ((x[i] == b[j]) && (i == j))
+					zz++;
+			}
+		}
 		printf("Kol-vo korov=%d", k);
-		printf("Kol-vo bikov=%d", z);
+		printf("Kol-vo bikov=%d", zz);
 	}
 	printf("Chislo ugadano!");
 }
